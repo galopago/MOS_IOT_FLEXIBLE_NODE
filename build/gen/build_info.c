9f50b494820a17bedd250b8bcc0246e4aa2aec3c
oportunidad <61303722+alnunez@users.noreply.github.com>
2022-01-26 22:06:28 -0500

    fixed bug with downlink commands, testing

--- build/gen/build_info.c
+++ build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20220127-003122";
-const char *build_timestamp = "2022-01-27T00:31:22Z";
+const char *build_id = "20220127-012938";
+const char *build_timestamp = "2022-01-27T01:29:38Z";
 const char *build_version = "1.0";
--- build/gen/mg_build_info.c
+++ build/gen/mg_build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *mg_build_id = "20220127-003120/2.19.1-gd52032f";
-const char *mg_build_timestamp = "2022-01-27T00:31:20Z";
+const char *mg_build_id = "20220127-012937/2.19.1-gd52032f";
+const char *mg_build_timestamp = "2022-01-27T01:29:37Z";
 const char *mg_build_version = "2.19.1";
